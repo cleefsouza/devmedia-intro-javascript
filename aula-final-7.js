6ed37b8c8d508f90028cb991dac76e0a21a5716d
cleefsouza <a.cleef.souza@gmail.com>
2019-01-19 02:10:05 -0300

    function, final-aula

--- aula-final-7.js
+++ aula-final-7.js
@@ -2,4 +2,24 @@ function imc(altura, peso) {
     return peso / (altura ** 2)
 }
 
-console.log('IMC atual: ', imc(1.78, 75))
+var pessoas = [
+    {
+        nome: 'pedro',
+        altura: 1.55,
+        peso: 51
+    },
+    {
+        nome: 'joão',
+        altura: 1.89,
+        peso: 74
+    },
+    {
+        nome: 'maria',
+        altura: 1.75,
+        peso: 62
+    }
+]
+
+for(var pessoa of pessoas){
+    console.log('IMC de', pessoa.nome,': ', imc(pessoa.altura, pessoa.peso))
+}
