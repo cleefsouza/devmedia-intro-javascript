3ccf45256b5418a0a1beb2675837d655822cbc8a
cleefsouza <a.cleef.souza@gmail.com>
2019-01-18 21:13:16 -0300

    aula-1

---
+++ aula-1.js
@@ -0,0 +1,9 @@
+var nome = 'cleefsouza'
+var idade = 23
+var altura = 1.78
+var maiorIdade = true
+
+console.log(nome, typeof nome)
+console.log(idade, typeof idade)
+console.log(altura, typeof altura)
+console.log(maiorIdade, typeof maiorIdade)
