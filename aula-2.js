03817f95ee99ee197d89672e8dee6bb67e209d15
cleefsouza <a.cleef.souza@gmail.com>
2019-01-18 21:26:26 -0300

    array e objeto

---
+++ aula-2.js
@@ -0,0 +1,24 @@
+// array
+var nomes = [
+    'cleef',
+    'jenifer',
+    'marcos',
+    'souza'
+]
+
+console.log(nomes[1])
+
+// objeto
+var pessoa = {
+    nome: 'cleef',
+    sobrenome: 'souza',
+    idade: 23,
+    amigos: ['wendeley',
+        'lucas',
+        'calixto',
+        'lynnick',
+        'alexandre']
+}
+
+console.log(pessoa.nome)
+console.log(pessoa.amigos[3])
